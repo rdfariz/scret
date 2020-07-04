2ec1caef16098b777c1dd047eda83b208bc9ecdf
rdfariz <rdfariz@student.telkomuniversity.ac.id>
2020-07-04 21:34:06 +0700

    fixing bug deploy v2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production '
+const dev = process.env.NODE_ENV !== 'production'
 const opts = {
   dev
 };
