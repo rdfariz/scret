c112f1880054a1a2bac054e4f48be6a3893c9cf2
rdfariz <rdfariz@student.telkomuniversity.ac.id>
2020-07-05 21:17:29 +0700

    upd help text

--- src/components/help/component.js
+++ src/components/help/component.js
@@ -4,8 +4,8 @@ export default class Component extends React.Component {
   render() {
     return (
       <div>
-        <p>1. Share your room ID with your friends</p>
-        <p>2. Join with the same id</p>
+        <p>1. Share your Room ID with your friends</p>
+        <p>2. Join with the same Room ID</p>
         <p>3. Write some code & share with your friends</p>
         <br></br>
         <span>made with ❤ by <a href="https://github.com/rdfariz" target="_blank">rd_fariz</a></span>
